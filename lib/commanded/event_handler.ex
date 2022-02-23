64ecf79246c2ef3d459d8e1d87ecac58702876d8
lee eggebroten <leggebroten@users.noreply.github.com>
2022-02-23 13:05:47 -0700

    Tests for Event Handler and updated OTLP attributes

--- lib/commanded/event_handler.ex
+++ lib/commanded/event_handler.ex
@@ -34,22 +34,28 @@ defmodule OpentelemetryCommanded.EventHandler do
 
   def handle_start(_event, _measurements, meta, _) do
     event = meta.recorded_event
+
+    # TODO: do we want to trap this with helpful message about needing Middleware?
     trace_headers = decode_headers(event.metadata["trace_ctx"])
     :otel_propagator_text_map.extract(trace_headers)
 
     attributes = [
-      "causation.id": event.causation_id,
-      "correlation.id": event.correlation_id,
-      "event.id": event.event_id,
-      "event.number": event.event_number,
-      "event.type": event.event_type,
-      "stream.id": event.stream_id,
-      "stream.version": event.stream_version,
-      application: meta.application,
+      "messaging.system": "commanded",
+      "messaging.protocol": "cqrs",
+      "messaging.destination_kind": "event_handler",
+      "messaging.operation": "receive",
+      "messaging.message_id": event.causation_id,
+      "messaging.conversation_id": event.correlation_id,
+      "messaging.destination": meta.handler_module,
+      "messaging.commanded.application": meta.application,
+      "messaging.commanded.event": event.event_type,
+      "messaging.commanded.event_id": event.event_id,
+      "messaging.commanded.event_number": event.event_number,
+      "messaging.commanded.stream_id": event.stream_id,
+      "messaging.commanded.stream_version": event.stream_version,
+      "messaging.commanded.handler_name": meta.handler_name
       # TODO add back
       # consistency: meta.consistency,
-      "handler.module": meta.handler_module,
-      "handler.name": meta.handler_name
       #  TODO add this back into commanded
       # "event.last_seen": meta.last_seen_event
     ]
---
+++ test/commanded/event_handler_test.exs
@@ -0,0 +1,149 @@
+defmodule OpentelemetryCommanded.EventHandlerTest do
+  use OpentelemetryCommanded.CommandedCase, async: false
+
+  import ExUnit.CaptureLog
+
+  alias OpentelemetryCommanded.DummyApp.Commands, as: C
+  alias OpentelemetryCommanded.DummyApp.Events, as: E
+
+  describe "dispatch command when Telemetry attached" do
+    setup _ do
+      case OpentelemetryCommanded.EventHandler.setup() do
+        :ok -> :ok
+        {:error, :already_exists} -> :ok
+      end
+    end
+
+    test "Success should create span", context do
+      :ok = app_dispatch(context, %C.Ok{id: "ACC123"})
+
+      assert_receive {:span,
+                      span(
+                        name: "commanded.event.handle",
+                        kind: :consumer,
+                        attributes: attributes
+                      )}
+
+      attributes = :otel_attributes.map(attributes)
+
+      has_basic_attributes!(attributes, context.correlation_id)
+
+      assert match?(
+               %{
+                 "messaging.commanded.event":
+                   "Elixir.OpentelemetryCommanded.DummyApp.Events.OkEvent"
+               },
+               attributes
+             )
+    end
+
+    test "Error should create span with error set", context do
+      _log =
+        capture_log(fn ->
+          :ok =
+            app_dispatch(context, %C.DoEvent{
+              id: "ACC123",
+              event: %E.ErrorInEventHandlerEvent{message: "some error"}
+            })
+        end)
+
+      assert_receive {:span,
+                      span(
+                        name: "commanded.event.handle",
+                        kind: :consumer,
+                        status: {:status, :error, exception_message},
+                        attributes: attributes
+                      )}
+
+      attributes = :otel_attributes.map(attributes)
+
+      has_basic_attributes!(attributes, context.correlation_id)
+
+      assert match?(
+               %{
+                 "messaging.commanded.event":
+                   "Elixir.OpentelemetryCommanded.DummyApp.Events.ErrorInEventHandlerEvent"
+               },
+               attributes
+             )
+
+      assert exception_message =~ "some error"
+    end
+
+    test "Exception should create span with error set", context do
+      _log =
+        capture_log(fn ->
+          :ok =
+            app_dispatch(context, %C.DoEvent{
+              id: "ACC123",
+              event: %E.ExceptionInEventHandlerEvent{message: "some error"}
+            })
+        end)
+
+      assert_receive {:span,
+                      span(
+                        name: "commanded.event.handle",
+                        kind: :consumer,
+                        status: {:status, :error, exception_message},
+                        attributes: attributes,
+                        events: events
+                      )}
+
+      attributes = :otel_attributes.map(attributes)
+
+      has_basic_attributes!(attributes, context.correlation_id)
+
+      assert match?(
+               %{
+                 "messaging.commanded.event":
+                   "Elixir.OpentelemetryCommanded.DummyApp.Events.ExceptionInEventHandlerEvent"
+               },
+               attributes
+             )
+
+      assert exception_message =~ "some error"
+
+      [
+        event(
+          name: "exception",
+          attributes: event_attributes
+        )
+      ] = :otel_events.list(events)
+
+      event_attributes = :otel_attributes.map(event_attributes)
+
+      assert match?(
+               %{
+                 "exception.message" => "some error",
+                 "exception.stacktrace" => _,
+                 "exception.type" => "Elixir.RuntimeError"
+               },
+               event_attributes
+             )
+
+      stack_trace = event_attributes["exception.stacktrace"]
+      assert stack_trace =~ "OpentelemetryCommanded.DummyApp.EventHandler.handle/2"
+    end
+  end
+
+  defp has_basic_attributes!(attributes, correlation_id) do
+    assert match?(
+             %{
+               "messaging.commanded.application": OpentelemetryCommanded.DummyApp.App,
+               "messaging.conversation_id": ^correlation_id,
+               "messaging.destination": OpentelemetryCommanded.DummyApp.EventHandler,
+               "messaging.destination_kind": "event_handler",
+               "messaging.message_id": _,
+               "messaging.operation": "receive",
+               "messaging.protocol": "cqrs",
+               "messaging.system": "commanded",
+               "messaging.commanded.event_id": _,
+               "messaging.commanded.event_number": 1,
+               "messaging.commanded.handler_name": "EventHandler",
+               "messaging.commanded.stream_id": "ACC123",
+               "messaging.commanded.stream_version": 1
+             },
+             attributes
+           )
+  end
+end
